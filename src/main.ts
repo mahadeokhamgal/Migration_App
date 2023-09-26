bef545c44f44dc1bb19a6f9219f93c2756817c3e
mahadeo.khamgal <mahadeo.khamgal@infosys.com>
2023-09-26 22:53:17 +0530

    Add ngjs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import ajsApp from './app/app.module.ajs';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
